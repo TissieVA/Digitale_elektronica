d1e5ea34abd1e59aeb9dab3a702c13c4930d22c4
TissieVA <tijs.van.alphen@hotmail.com>
2019-11-05 21:37:45 +0100

    ADD: randomDriehoek

--- project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/driehoek.vhd
+++ project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/driehoek.vhd
@@ -35,4 +35,41 @@ component Pixel_Pulser_V
         );
 end component;
 
+component ClockingWizard
+port
+ (-- Clock in ports
+  -- Clock out ports
+  PixelClk          : out    std_logic;
+  Clk100MHz           : in     std_logic
+ );
+end component;
+
+COMPONENT TriangleFifo
+  PORT (
+    clk : IN STD_LOGIC;
+    srst : IN STD_LOGIC;
+    din : IN STD_LOGIC_VECTOR(58 DOWNTO 0);
+    wr_en : IN STD_LOGIC;
+    rd_en : IN STD_LOGIC;
+    dout : OUT STD_LOGIC_VECTOR(58 DOWNTO 0);
+    full : OUT STD_LOGIC;
+    empty : OUT STD_LOGIC
+  );
+END COMPONENT;
+
+COMPONENT VideoMemory
+  PORT (
+    clka : IN STD_LOGIC;
+    wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+    addra : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+    dina : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    douta : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    clkb : IN STD_LOGIC;
+    web : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+    addrb : IN STD_LOGIC_VECTOR(18 DOWNTO 0);
+    dinb : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    doutb : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+  );
+END COMPONENT;
+
 end package;
--- project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/project_Driehoeken.vhd
+++ project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/project_Driehoeken.vhd
@@ -4,15 +4,77 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use work.driehoek.ALL;
 
 
 entity project_Driehoeken is
---  Port ( );
+    Port(
+        CLK100MHZ: in std_logic
+    );
+
 end project_Driehoeken;
 
 architecture Behavioral of project_Driehoeken is
 
+signal color: std_logic:='0';
+signal full, wr_en, rd_en: std_logic:='0';
+signal empty: std_logic:='1';
+signal sequence: std_logic_vector(15 downto 0);
+signal din, dout: std_logic_vector(58 downto 0);
+signal pixelClk: std_logic;
+signal FifoInCounter: integer:=0;
+
 begin
 
+Clock: ClockingWizard
+    port map(
+        Clk100Mhz => CLK100MHZ,
+        PixelClk => Pixelclk
+        );
+        
+
+Fifo : TriangleFifo
+
+    Port map (
+    clk => pixelclk,
+    srst => '0',
+    din => din,
+    wr_en => wr_en,
+    rd_en => rd_en,
+    dout => dout,
+    full => full,
+    empty => empty
+    );
+    
+pmLFSR : LFSR
+    port map(
+        sequence_out => sequence,
+        clk => pixelclk
+    );
+
+
+pFifo: process(pixelClk,FifoInCounter)
+begin
+
+if(rising_edge(pixelClk)) then
+    
+    if(FifoInCounter=0) then
+    
+        din(58 downto 43) <= sequence(15 downto 0);
+     
+     elsif (FifoInCounter = 1) then
+        din(42 downto 27) <= sequence;
+        
+    elsif (FifoInCounter=2) then
+        din(26 downto 11) <= sequence;
+        
+    elsif (FifoInCounter=3) then
+        din(10 downto 2) <= sequence(16 downto 9);
+    
+    end if;
+    
+end if;
+
+end process;
 
 end Behavioral;
