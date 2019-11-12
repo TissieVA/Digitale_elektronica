7f20b6642bd92fcea595a1e3e17ab3056d8c15eb
TissieVA <tijs.van.alphen@hotmail.com>
2019-11-12 09:53:57 +0100

    ADD: randomdriehoek

---
+++ project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/TrianglePrepare.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/07/2019 10:58:06 AM
+-- Design Name: 
+-- Module Name: TrianglePrepare - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TrianglePrepare is
+--  Port ( );
+end TrianglePrepare;
+
+architecture Behavioral of TrianglePrepare is
+
+begin
+
+
+end Behavioral;
--- project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/project_Driehoeken.vhd
+++ project_RandomDriehoek/project_RandomDriehoek.srcs/sources_1/new/project_Driehoeken.vhd
@@ -1,5 +1,13 @@
 --Tijs Van Alphen
 
+-- 58 downto 49 is x cor 1
+-- 48 downto 39 is x cor 2
+-- 38 downto 29 is x cor 3
+-- 28 downto 20 is y cor 1
+-- 19 downto 11 is y cor 2
+-- 10 downto 2 is y cor 3
+-- 1 is color
+-- 0 i last
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -22,7 +30,10 @@ signal empty: std_logic:='1';
 signal sequence: std_logic_vector(15 downto 0);
 signal din, dout: std_logic_vector(58 downto 0);
 signal pixelClk: std_logic;
-signal FifoInCounter: integer:=0;
+signal FifoInCounter, BresenhamCounter: integer:=0;
+signal x0, x1, y0, y1, x_out, y_out: integer;
+signal Start_in, Plot: std_logic;
+
 
 begin
 
@@ -52,15 +63,30 @@ pmLFSR : LFSR
         clk => pixelclk
     );
 
+pmBresenham : Bresenham
+    Port Map (
+        x0 => x0,
+        x1 => x1,
+        y0 => y0,
+        y1 => y1,
+        start_in => start_in,
+        Clk => pixelclk,
+        X_out =>x_out,
+        Y_out => y_out,
+        Plot => Plot
+    
+    );
+
 
 pFifo: process(pixelClk,FifoInCounter)
 begin
+
 
 if(rising_edge(pixelClk)) then
     
     if(FifoInCounter=0) then
     
-        din(58 downto 43) <= sequence(15 downto 0);
+        din(58 downto 43) <= sequence;
      
      elsif (FifoInCounter = 1) then
         din(42 downto 27) <= sequence;
@@ -70,11 +96,39 @@ if(rising_edge(pixelClk)) then
         
     elsif (FifoInCounter=3) then
         din(10 downto 2) <= sequence(16 downto 9);
+        FifoInCounter <=0;
+        rd_en <= '1';
     
     end if;
-    
-end if;
+   end if;
 
 end process;
+
+pPrepareFrame: process(dout)
+ begin
+ 
+    if(rd_en ='1') then
+        rd_en <='0';
+        Start_in <= '1';
+        
+        if(BresenhamCounter =0 ) then
+        x0<= to_integer(signed(dout(58 downto 49)));   
+        x1<= to_integer(signed(dout(48 downto 39)));
+        y0<= to_integer(signed(dout(28 downto 20)));
+        y1<= to_integer(signed(dout(19 downto 10)));
+        
+        BresenhamCounter <= BresenhamCounter+1; 
+        
+        elsif(BresenhamCounter = 1) then
+        x0<= to_integer(signed(48 downto 39));
+        x1 <= to_integer(signed(38 downto 29));
+        y0 <= to_integer()
+        
+    
+    end if;
+ 
+ end process;
+
+
 
 end Behavioral;
